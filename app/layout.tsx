20fb90822a025587f5599e79c96d61b33fe46152
AdamAzuddin <05adamaz@gmail.com>
2023-05-10 20:23:12 +0800

    create new folders and add new page and layout.tsx file

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import '@styles/globals.css'
+
+export const metadata = {
+    title:"fMovie",
+    description: "Find and save movies to watch with friends and family"
+}
+
+const Layout = ({children}:any) => {
+  return (
+    <html lang='en'>
+        <body>
+            <main>
+                {children}
+            </main>
+        </body>
+    </html>
+  )
+}
+
+export default Layout
