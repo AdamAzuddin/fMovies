21ca9a31fe982ad2bd2e6b4ef2a64a2781ce7772
AdamAzuddin <05adamaz@gmail.com>
2023-05-19 22:17:22 +0800

    add row

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import "@styles/globals.css";
 import MovieBanner from "@components/MovieBanner";
+import Row from '@components/Row'
 import requests from "@utils/requests";
 
 async function getData(){
@@ -42,13 +43,30 @@ async function getData(){
 const Home = async() => {
   const data = await getData()
   const netflixOriginals=data.props.netflixOriginals
-  
+  const trendingNow=data.props.trendingNow
+  const topRated=data.props.topRated
+  const actionMovies=data.props.actionMovies
+  const comedyMovies=data.props.comedyMovies
+  const horrorMovies=data.props.horrorMovies
+  const romanceMovies=data.props.romanceMovies
+  const documentaries=data.props.documentaries
 
   return (
     <div className="container mx-auto py-6">
       <MovieBanner
         netflixOriginals={netflixOriginals}
       />
+      <section className="md:space-y-24">
+          <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
+          {/* My List component */}
+
+          <Row title="Comedies" movies={comedyMovies} />
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
+        </section>
     </div>
   );
 };
