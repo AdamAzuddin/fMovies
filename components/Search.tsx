d95b1eb4abcaa68cd5ebaa053117891be7b07509
AdamAzuddin <05adamaz@gmail.com>
2023-05-22 10:15:02 +0800

    add search suggestion with custom scrollbar

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,30 +1,68 @@
+import { useState } from "react";
 
 export default function Search() {
-    return (
-        <div className="flex items-center">
-            <div className="flex space-x-1">
-                <input
-                    type="text"
-                    className="block w-full px-4 py-1 border rounded-full focus:outline-none text-sm md:text-xl text-black"
-                    placeholder="Search for anything"
-                />
-                <button className="px-4 text-white rounded-full ">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="header-icon"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        strokeWidth={2}
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round" 
-                            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                        />
-                    </svg>
-                </button>
-            </div>
-        </div>
+  const [searchTerm, setSearchTerm] = useState("");
+  const [suggestions, setSuggestions] = useState<string[]>([]);
+
+  const suggestionList = [
+    "Suggestion 1",
+    "Suggestion 2",
+    "Suggestion 3",
+    "Suggestion 1",
+    "Suggestion 2",
+    "Suggestion 3",
+    "Suggestion 1",
+    "Suggestion 2",
+    "Suggestion 3",
+  ];
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setSearchTerm(value);
+
+    // Filter the suggestion list based on the search term
+    const filteredSuggestions = suggestionList.filter((suggestion) =>
+      suggestion.toLowerCase().includes(value.toLowerCase())
     );
+    setSuggestions(filteredSuggestions);
+  };
+
+  return (
+    <div className="relative items-center">
+      <div className="flex space-x-1">
+        <input
+          type="text"
+          className="block w-full px-4 py-1 border rounded-full focus:outline-none text-sm md:text-xl text-black"
+          placeholder="Search for anything"
+          value={searchTerm}
+          onChange={handleInputChange}
+        />
+        <button className="px-4 text-white rounded-full ">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="header-icon"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
+        </button>
+      </div>
+      <div className="absolute top-12 overflow-y-auto max-h-40 !scrollbar-track-transparent !scrollbar-thumb-slate-700 !scrollbar-thin w-full">
+        {suggestions.map((suggestion, index) => (
+          <div key={index} className="text-xs md:text-xl lg:text-2xl flex">
+            
+            <img src="http://goo.gl/vyAs27" alt="image"/>
+            <p>{suggestion}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
