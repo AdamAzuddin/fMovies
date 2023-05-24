f7f9869a4a3eca94e984643d59e05a95a83cb0ac
AdamAzuddin <05adamaz@gmail.com>
2023-05-24 12:37:59 +0800

    can click to thumbnail and navigate to /details/[movieId]

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,13 +1,17 @@
 import { Movie } from "@typings";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 interface Props {
   movie: Movie;
 }
 
 const Thumbnail = ({ movie }: Props) => {
+  const router= useRouter()
+
+ 
   return (
-    <div className="bg-black cursor-pointer h-[13rem] md:h-[15rem] lg:h-72">
+    <div className="bg-black cursor-pointer h-[13rem] md:h-[15rem] lg:h-72" onClick={()=> router.push(`/details/${movie.id}`)}>
       <div className="relative h-28 min-w-[56px] transition duration-200 ease-out md:h-32 md:min-w-[64px] lg:h-44 lg:min-w-[88px]">
         <Image
           src={`https://image.tmdb.org/t/p/w500${
