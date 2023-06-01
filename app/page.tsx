ea40e07d86d2ba493c32761521ca6eee0611728d
AdamAzuddin <05adamaz@gmail.com>
2023-06-01 22:36:28 +0800

    refactor fetch method on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,20 @@
 import "@styles/globals.css";
 import MovieBanner from "@components/MovieBanner";
+import movie from "@utils/requests/movie"
+import series from "@utils/requests/series"
 import Row from "@components/Row";
 import requests from "@utils/requests";
 
 async function getData() {
   const [
-    netflixOriginals,
-    trendingNow,
-    topRated,
-    actionMovies,
-    comedyMovies,
-    horrorMovies,
-    romanceMovies,
-    documentaries,
+    netflixOriginals
   ] = await Promise.all([
     fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
-    fetch(requests.fetchTrending).then((res) => res.json()),
-    fetch(requests.fetchTopRated).then((res) => res.json()),
-    fetch(requests.fetchActionMovies).then((res) => res.json()),
-    fetch(requests.fetchComedyMovies).then((res) => res.json()),
-    fetch(requests.fetchHorrorMovies).then((res) => res.json()),
-    fetch(requests.fetchRomanceMovies).then((res) => res.json()),
-    fetch(requests.fetchDocumentaries).then((res) => res.json()),
   ]);
 
   return {
     props: {
       netflixOriginals: netflixOriginals.results,
-      trendingNow: trendingNow.results,
-      topRated: topRated.results,
-      actionMovies: actionMovies.results,
-      comedyMovies: comedyMovies.results,
-      horrorMovies: horrorMovies.results,
-      romanceMovies: romanceMovies.results,
-      documentaries: documentaries.results,
     },
   };
 }
@@ -41,26 +22,28 @@ async function getData() {
 const Home = async () => {
   const data = await getData();
   const netflixOriginals = data.props.netflixOriginals;
-  const trendingNow = data.props.trendingNow;
-  const topRated = data.props.topRated;
-  const actionMovies = data.props.actionMovies;
-  const comedyMovies = data.props.comedyMovies;
-  const horrorMovies = data.props.horrorMovies;
-  const romanceMovies = data.props.romanceMovies;
-  const documentaries = data.props.documentaries;
+  const trendingNow = await movie.fetchTrending();
+  const topRated = await movie.fetchTopRated();
+  const actionMovies = await movie.fetchActionMovies();
+  const comedyMovies = await movie.fetchComedyMovies();
+  const animationSeries = await series.fetchAnimationeSeriesData()
+  const horrorMovies = await movie.fetchHorrorMovies();
+  const romanceMovies = await movie.fetchRomanceMovies();
+  const documentaries = await movie.fetchDocumentaries();
 
   return (
     <div className="container mx-auto py-6">
       <MovieBanner netflixOriginals={netflixOriginals} />
 
       <section className="md:space-y-24">
-        <Row title="Trending Now" movies={trendingNow} />
-        <Row title="Top Rated" movies={topRated} />
-        <Row title="Action Thrillers" movies={actionMovies} />
-        <Row title="Comedies" movies={comedyMovies} />
-        <Row title="Scary Movies" movies={horrorMovies} />
-        <Row title="Romance Movies" movies={romanceMovies} />
-        <Row title="Documentaries" movies={documentaries} />
+      <Row title="Trending Now" movies={trendingNow.results} />
+      <Row title="Top Rated Movies" movies={topRated.results} />
+      <Row title="Action Thrillers" movies={actionMovies.results} />
+      <Row title="Comedies" movies={comedyMovies.results} />
+      <Row title="Animation Series" movies={animationSeries.results} />
+      <Row title="Scary Movies" movies={horrorMovies.results} />
+      <Row title="Romance Movies" movies={romanceMovies.results} />
+      <Row title="Documentaries" movies={documentaries.results} /> 
       </section>
     </div>
   );
