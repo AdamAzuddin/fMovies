383a157800ff121849c793b8fba0301e3ddfed43
AdamAzuddin <05adamaz@gmail.com>
2023-06-01 16:57:16 +0800

    add genres like animation, comedy, drama, etc

--- app/series/page.tsx
+++ app/series/page.tsx
@@ -1,63 +1,28 @@
 import "@styles/globals.css";
 import Row from "@components/Row";
-import requests from "@utils/requests";
 import series from "@utils/requests/series"
 
-async function getData() {
-  const [
-    trendingNow,
-    topRated,
-    actionMovies,
-    comedyMovies,
-    horrorMovies,
-    romanceMovies,
-    documentaries,
-  ] = await Promise.all([
-    fetch(requests.fetchTrending).then((res) => res.json()),
-    fetch(requests.fetchTopRated).then((res) => res.json()),
-    fetch(requests.fetchActionMovies).then((res) => res.json()),
-    fetch(requests.fetchComedyMovies).then((res) => res.json()),
-    fetch(requests.fetchHorrorMovies).then((res) => res.json()),
-    fetch(requests.fetchRomanceMovies).then((res) => res.json()),
-    fetch(requests.fetchDocumentaries).then((res) => res.json()),
-  ]);
-
-  return {
-    props: {
-      trendingNow: trendingNow.results,
-      topRated: topRated.results,
-      actionMovies: actionMovies.results,
-      comedyMovies: comedyMovies.results,
-      horrorMovies: horrorMovies.results,
-      romanceMovies: romanceMovies.results,
-      documentaries: documentaries.results,
-    },
-  };
-}
-
 const SeriesPage = async() => {
-    const data = await getData();
-    const trendingNow = data.props.trendingNow;
-    const topRated = data.props.topRated;
-    const actionMovies = data.props.actionMovies;
-    const comedyMovies = data.props.comedyMovies;
-    const horrorMovies = data.props.horrorMovies;
-    const romanceMovies = data.props.romanceMovies;
-    const documentaries = data.props.documentaries;
 
-    const seriesData = await series.fetchTopRatedSeriesData()
-  
+    const topRated = await series.fetchTopRatedSeriesData()
+    const trending = await series.fetchTrendingSeriesData()
+    const actionAndAdventure = await series.fetchActionAdventureSeriesData()
+    const animation = await series.fetchAnimationeSeriesData()
+    const comedy = await series.fetchComedySeriesData()
+    const drama = await series.fetchDramaSeriesData()
+    const family = await series.fetchFamilySeriesData()
+
     return (
       <div className="container mx-auto py-6">
-  
+
         <section className="md:space-y-24">
-          <Row title="Trending Now" movies={trendingNow} />
-          <Row title="Top Rated" movies={seriesData.results} />
-          <Row title="Action Thrillers" movies={actionMovies} />
-          <Row title="Comedies" movies={comedyMovies} />
-          <Row title="Horror" movies={horrorMovies} />
-          <Row title="Romance" movies={romanceMovies} />
-          <Row title="Documentaries" movies={documentaries} />
+          <Row title="Trending Today" movies={trending.results}/>
+          <Row title="Top Rated" movies={topRated.results} />
+          <Row title="Action & Adventure" movies={actionAndAdventure.results} />
+          <Row title="Animation" movies={animation.results} />
+          <Row title="Comedy" movies={comedy.results} />
+          <Row title="Drama" movies={drama.results} />
+          <Row title="Family" movies={family.results} />
         </section>
       </div>
     );
--- utils/requests/series.js
+++ utils/requests/series.js
@@ -4,15 +4,68 @@ const BASE_URL = 'https://api.themoviedb.org/3'
 export default {
     async fetchTopRatedSeriesData() {
         const res = await fetch(`${BASE_URL}/tv/top_rated?api_key=${API_KEY}&language=en-US`);
-        // The return value is *not* serialized
-        // You can return Date, Map, Set, etc.
-       
-        // Recommendation: handle errors
+        
         if (!res.ok) {
-          // This will activate the closest `error.js` Error Boundary
           throw new Error('Failed to fetch data');
         }
        
         return res.json();
-      }
+      },
+
+      async fetchTrendingSeriesData() {
+        const res = await fetch(`${BASE_URL}/trending/tv/day?api_key=${API_KEY}&language=en-US`);
+        
+        if (!res.ok) {
+          throw new Error('Failed to fetch data');
+        }
+       
+        return res.json();
+      },
+
+      async fetchActionAdventureSeriesData() {
+        const res = await fetch(`${BASE_URL}/discover/tv?api_key=${API_KEY}&language=en-US&with_genres=10759`);
+        
+        if (!res.ok) {
+          throw new Error('Failed to fetch data');
+        }
+       
+        return res.json();
+      },
+      async fetchAnimationeSeriesData() {
+        const res = await fetch(`${BASE_URL}/discover/tv?api_key=${API_KEY}&language=en-US&with_genres=16`);
+        
+        if (!res.ok) {
+          throw new Error('Failed to fetch data');
+        }
+       
+        return res.json();
+      },
+      async fetchComedySeriesData() {
+        const res = await fetch(`${BASE_URL}/discover/tv?api_key=${API_KEY}&language=en-US&with_genres=35`);
+        
+        if (!res.ok) {
+          throw new Error('Failed to fetch data');
+        }
+       
+        return res.json();
+      },
+      async fetchDramaSeriesData() {
+        const res = await fetch(`${BASE_URL}/discover/tv?api_key=${API_KEY}&language=en-US&with_genres=18`);
+        
+        if (!res.ok) {
+          throw new Error('Failed to fetch data');
+        }
+       
+        return res.json();
+      },
+      async fetchFamilySeriesData() {
+        const res = await fetch(`${BASE_URL}/discover/tv?api_key=${API_KEY}&language=en-US&with_genres=10751`);
+        
+        if (!res.ok) {
+          throw new Error('Failed to fetch data');
+        }
+       
+        return res.json();
+      },
+
 }
