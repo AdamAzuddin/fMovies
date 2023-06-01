ba562f25e809d23eb9e855dd25b7668c39a0d200
AdamAzuddin <05adamaz@gmail.com>
2023-06-01 22:16:11 +0800

    use new fetch method

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,61 +1,27 @@
 import "@styles/globals.css";
-import MovieBanner from "@components/MovieBanner";
 import Row from "@components/Row";
-import requests from "@utils/requests";
-
-async function getData() {
-  const [
-    trendingNow,
-    topRated,
-    actionMovies,
-    comedyMovies,
-    horrorMovies,
-    romanceMovies,
-    documentaries,
-  ] = await Promise.all([
-    fetch(requests.fetchTrending).then((res) => res.json()),
-    fetch(requests.fetchTopRated).then((res) => res.json()),
-    fetch(requests.fetchActionMovies).then((res) => res.json()),
-    fetch(requests.fetchComedyMovies).then((res) => res.json()),
-    fetch(requests.fetchHorrorMovies).then((res) => res.json()),
-    fetch(requests.fetchRomanceMovies).then((res) => res.json()),
-    fetch(requests.fetchDocumentaries).then((res) => res.json()),
-  ]);
-
-  return {
-    props: {
-      trendingNow: trendingNow.results,
-      topRated: topRated.results,
-      actionMovies: actionMovies.results,
-      comedyMovies: comedyMovies.results,
-      horrorMovies: horrorMovies.results,
-      romanceMovies: romanceMovies.results,
-      documentaries: documentaries.results,
-    },
-  };
-}
+import movie from "@utils/requests/movie"
 
 const MoviesPage = async() => {
-    const data = await getData();
-    const trendingNow = data.props.trendingNow;
-    const topRated = data.props.topRated;
-    const actionMovies = data.props.actionMovies;
-    const comedyMovies = data.props.comedyMovies;
-    const horrorMovies = data.props.horrorMovies;
-    const romanceMovies = data.props.romanceMovies;
-    const documentaries = data.props.documentaries;
+  const trending = await movie.fetchTrending()
+  const topRated = await movie.fetchTopRated()
+  const action = await movie.fetchActionMovies()
+  const comedy = await movie.fetchComedyMovies()
+  const horror = await movie.fetchHorrorMovies()
+  const documentaries = await movie.fetchDocumentaries()
+  const upcoming = await movie.fetchUpcoming() 
   
     return (
       <div className="container mx-auto py-6">
   
         <section className="md:space-y-24">
-          <Row title="Trending Now" movies={trendingNow} />
-          <Row title="Top Rated" movies={topRated} />
-          <Row title="Action Thrillers" movies={actionMovies} />
-          <Row title="Comedies" movies={comedyMovies} />
-          <Row title="Horror" movies={horrorMovies} />
-          <Row title="Romance" movies={romanceMovies} />
-          <Row title="Documentaries" movies={documentaries} />
+          <Row title="Trending" movies={trending.results}/>
+          <Row title="Top Rated" movies={topRated.results}/>
+          <Row title="Coming Soon" movies={upcoming.results}/>
+          <Row title="Action" movies={action.results}/>
+          <Row title="Comedy" movies={comedy.results}/>
+          <Row title="Horror" movies={horror.results}/>
+          <Row title="Documentaries" movies={documentaries.results}/>
         </section>
       </div>
     );
---
+++ utils/requests/movie.js
@@ -0,0 +1,80 @@
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY
+const BASE_URL = 'https://api.themoviedb.org/3'
+
+export default {
+    async fetchTrending() {
+        const res = await fetch(`${BASE_URL}/trending/all/week?api_key=${API_KEY}&language=en-US`);
+        
+        if (!res.ok) {
+          throw new Error('Failed to fetch data');
+        }
+       
+        return res.json();
+      },
+
+      async fetchTopRated() {
+        const res = await fetch(`${BASE_URL}/movie/top_rated?api_key=${API_KEY}&language=en-US`);
+        
+        if (!res.ok) {
+          throw new Error('Failed to fetch data');
+        }
+       
+        return res.json();
+      },
+
+      async fetchActionMovies() {
+        const res = await fetch(`${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=28`);
+        
+        if (!res.ok) {
+          throw new Error('Failed to fetch data');
+        }
+       
+        return res.json();
+      },
+      async fetchComedyMovies() {
+        const res = await fetch(`${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=35`);
+        
+        if (!res.ok) {
+          throw new Error('Failed to fetch data');
+        }
+       
+        return res.json();
+      },
+      async fetchHorrorMovies() {
+        const res = await fetch(`${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=27`);
+        
+        if (!res.ok) {
+          throw new Error('Failed to fetch data');
+        }
+       
+        return res.json();
+      },
+      async fetchRomanceMovies() {
+        const res = await fetch(`${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=10749`);
+        
+        if (!res.ok) {
+          throw new Error('Failed to fetch data');
+        }
+       
+        return res.json();
+      },
+      async fetchDocumentaries() {
+        const res = await fetch(`${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=99`);
+        
+        if (!res.ok) {
+          throw new Error('Failed to fetch data');
+        }
+       
+        return res.json();
+      },
+      async fetchUpcoming() {
+        const res = await fetch(`${BASE_URL}/movie/upcoming?api_key=${API_KEY}&language=en-US&with_genres=99`);
+        
+        if (!res.ok) {
+          throw new Error('Failed to fetch data');
+        }
+       
+        return res.json();
+      },
+
+}
