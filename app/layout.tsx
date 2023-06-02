2be16e8079657e2a16e7cc23ef7e0dab0c362e53
AdamAzuddin <05adamaz@gmail.com>
2023-06-02 15:49:22 +0800

    add type PropsWithChildren in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,14 @@
 import "@styles/globals.css";
 import NavHeader from "@components/NavHeader";
 import Provider from "@components/Provider";
+import { PropsWithChildren } from "react";
 
 export const metadata = {
   title: "fMovie",
   description: "Find and save movies to watch with friends and family",
 };
 
-const Layout = ({ children }: any) => {
+const Layout = ({ children }: PropsWithChildren) => {
   return (
     <html lang="en">
 
