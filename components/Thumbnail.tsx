2e35547e246e507c69bbcf795f14f3d2c9056a7d
AdamAzuddin <05adamaz@gmail.com>
2023-06-02 19:00:16 +0800

    add todos

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -3,10 +3,12 @@ import { useRouter } from "next/navigation";
 import useDetails from "@hooks/useDetails";
 import { ThumbnailProps } from "@typings";
 
+//TODO: Add watch later, favourites and watched button
+//TODO: Create a new file in /utils/functions/[functionName] that hold function of watch later, favourites and watched button
+
 const Thumbnail = ({ movie }: ThumbnailProps) => {
   const router = useRouter();
 
-  const jsonData = movie;
   const jsonString =JSON.stringify(movie)
   // access setJsonDara function from the useDetails Hook
   const setJsonData = useDetails((state)=> state.setJsonData)
