21c93f341a4349c94d75d39226267d944f354431
AdamAzuddin <05adamaz@gmail.com>
2023-06-07 13:37:18 +0800

    change favourites page to jsx and able to get db data of favourites model

---
+++ app/favourites/page.jsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { useSession, getProviders } from "next-auth/react";
+import { useState, useEffect } from "react";
+import Thumbnail from "@components/Thumbnail";
+
+const FavouritesPage = async () => {
+  
+  const { data: session } = useSession();
+  const [providers, setProviders] = useState(null);
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const res = await getProviders();
+      setProviders(res);
+    })();
+  }, []);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(
+        `/api/users/${session?.user.id}/favourites`
+      );
+      const res = await response.json();
+
+      const jsonArray = [];
+      for (let index = 0; index < res.length; index++) {
+        const element = res[index];
+        const dataElement = JSON.parse(element.data);
+        jsonArray.push(dataElement);
+      }
+
+      setList(jsonArray);
+      console.log(jsonArray);
+    };
+
+    if (session?.user.id) {
+      fetchPosts();
+    }
+  }, [session?.user.id]);
+
+  return (
+    <div>
+      <h1>Favourites</h1>
+      {session?.user ? (
+        <>
+          <div className="flex space-x-4">
+            {list.map((item, index) => (
+              <div key={index}>
+                <Thumbnail movie={item}/>
+              </div>
+            ))}
+          </div>
+        </>
+      ) : (
+        <div>Sign in to view your watch later list</div>
+      )}
+    </div>
+  );
+};
+
+export default FavouritesPage;
