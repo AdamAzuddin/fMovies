d11cad8b77c1a363655face0580a06e05242a9ed
AdamAzuddin <05adamaz@gmail.com>
2023-06-07 13:34:35 +0800

    delete List compoenent

--- components/List.tsx
+++
@@ -1,9 +0,0 @@
-// TODO: Map through the array, convert the string to json and show thumbnail for each json object. 
-//If None, write:  No movies on this list yet.
-const List = (dataArray: String) => {
-  return (
-    <div>List</div>
-  )
-}
-
-export default List
