ab86d54acafa1ed6bdecb5b131733765e896833a
AdamAzuddin <05adamaz@gmail.com>
2023-06-12 12:39:19 +0800

    create comment schema

--- models/comment.js
+++ models/comment.js
@@ -1,1 +1,21 @@
-//TODO: Create Comment models
+import { Schema, model, models } from 'mongoose';
+
+const CommentSchema = new Schema({
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  username: {
+    type: String
+  },
+  image: {
+    type: String
+  },
+  text: {
+    type: String
+  }
+});
+
+const Comment = models.Comment || model('Comment', CommentSchema);
+
+export default Comment;
